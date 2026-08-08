1ce7229f36f77faf47caba28a8ea3294616758a6
reyclouddev-ops <reyclouddev@gmail.com>
2026-08-08 19:53:46 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  BOT_TOKEN: "YOUR_BOT_TOKEN",
+  OWNER_ID: "YOUR_ID",
+
+  BOT_NAME: "ReyCloud",
+  BOT_VERSION: "1.0.0",
+
+  CHANNEL_USERNAME: "@yourchannel",
+  CHANNEL_URL: "https://t.me/yourchannel",
+
+  LOG_CHANNEL_ID: "YOUR_LOG_CHANNEL_ID",
+
+  VERCEL_TOKEN: "YOUR_VERCEL_TOKEN",
+  VERCEL_DOMAIN: "legionteknologi.my.id"
+}
